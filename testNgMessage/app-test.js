4a72d03916d0917b6dd8391dfd31336a1af89cc2
httran <hoanh.tran@gdit.com>
2018-02-09 12:23:03 -0800

    ng msg tests

---
+++ testNgMessage/app-test.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Deb on 8/20/2014.
+ */
+(function () {
+    "use strict";
+    var app = angular.module("onlineClassSchedulerXX",
+           [
+            //   "common.services",
+            //"common.directives",
+            //"ngRoute",
+            "ngMessages",
+            //"classResourceMock"
+           ]);
+
+}());
