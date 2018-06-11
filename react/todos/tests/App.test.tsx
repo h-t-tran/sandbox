d1188ce8cd029d3fa3d9ee0c501b3f81d048c2c7
httran <hoanh.tran@gdit.com>
2018-06-11 13:53:36 -0700

    add another TS seed

---
+++ react/todos/tests/App.test.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as TestUtils from 'react-dom/test-utils';
+
+it('2 should be equal to 2', () => {
+    //
+    // Arrange
+    //
+
+    //
+    // Act
+    //
+
+    //
+    // Assert
+    //
+    expect(2).toBe(2);
+});
+
+it('object should not be null', () => {
+    //
+    // Arrange
+    //
+    let o = {};
+    //
+    // Act
+    //
+
+    //
+    // Assert
+    //
+    expect(o).not.toBe(null);
+});
