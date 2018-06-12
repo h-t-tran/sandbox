bde900ac4d7f539173ac4f1b97a52413aa54d630
hoanh.tran <hoanh.tran@gdit.com>
2018-06-12 13:09:48 -0700

    save good

--- react/todos/tests/App.test.tsx
+++ react/todos/tests/App.test.tsx
@@ -1,43 +1,21 @@
 import * as React from 'react';
-//import * as ReactDOM from 'react-dom';
 import * as TestUtils from 'react-dom/test-utils';
-//import { App as TodoApp } from '../app/containers/App';
-import Foobar from "../src/homepage/Foobar";
-
-it('2 should be equal to 2', () => {
-    //
-    // Arrange
-    //
-
-    //
-    // Act
-    //
-
-    //
-    // Assert
-    //
-    expect(2).toBe(2);
-});
-
+import Foobar from '../src/homepage/Foobar';
+import * as ReactDOM from 'react-dom';
 
 it('App is rendered', () => {
-  //
-  // Arrange
-  //
 
-  //
-  // Act
-  //
   // Render App in the document
   const appElement: any = TestUtils.renderIntoDocument(
     <Foobar/>
   );
   //
-  // const appNode = ReactDOM.findDOMNode(appElement);
+  const appNode = ReactDOM.findDOMNode(appElement);
+
+  console.log("appNode.textContent = ", appNode.textContent);
   //
-  // //
-  // // Assert
-  // //
-  //
-  expect(appElement.textContent).not.toBe(null);
+  // // Verify text content
+  expect(appNode.textContent).not.toBe(null);
 });
+
+
---
+++ react/todos/tests/App2.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import * as TestUtils from 'react-dom/test-utils';
+import Foobar from '../src/homepage/Foobar';
+
+it('App is rendered', () => {
+
+  // Render App in the document
+  // const appElement: Foobar = TestUtils.renderIntoDocument(
+  //   <Foobar/>
+  // );
+  // //
+  // const appNode = ReactDOM.findDOMNode(appElement);
+  //
+  // console.log("appNode.textContent = ", appNode.textContent);
+  //
+  // // Verify text content
+  expect(2).not.toBe(null);
+});
+
+
