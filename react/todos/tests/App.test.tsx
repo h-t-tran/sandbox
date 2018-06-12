fcec89c32d91a734f2fc960a08f6a75537e202e5
hoanh.tran <hoanh.tran@gdit.com>
2018-06-12 10:37:57 -0700

    able to run 'test'

--- react/todos/tests/App.test.tsx
+++ react/todos/tests/App.test.tsx
@@ -1,8 +1,10 @@
-// import * as React from 'react';
-// import * as ReactDOM from 'react-dom';
-// import * as TestUtils from 'react-dom/test-utils';
+import * as React from 'react';
+//import * as ReactDOM from 'react-dom';
+import * as TestUtils from 'react-dom/test-utils';
+//import { App as TodoApp } from '../app/containers/App';
+import Foobar from "../src/homepage/Foobar";
 
-//it('2 should be equal to 2', () => {
+it('2 should be equal to 2', () => {
     //
     // Arrange
     //
@@ -14,20 +16,28 @@
     //
     // Assert
     //
-    //expect(2).toBe(2);
-//});
-//
-// it('object should not be null', () => {
-//     //
-//     // Arrange
-//     //
-//     let o = {};
-//     //
-//     // Act
-//     //
-//
-//     //
-//     // Assert
-//     //
-//     expect(o).not.toBe(null);
-// });
+    expect(2).toBe(2);
+});
+
+
+it('App is rendered', () => {
+  //
+  // Arrange
+  //
+
+  //
+  // Act
+  //
+  // Render App in the document
+  const appElement: any = TestUtils.renderIntoDocument(
+    <Foobar/>
+  );
+  //
+  // const appNode = ReactDOM.findDOMNode(appElement);
+  //
+  // //
+  // // Assert
+  // //
+  //
+  expect(appElement.textContent).not.toBe(null);
+});
